765b72803c2e525b6a71cba5b5f17448cd3be493
Pathfinder <michalmpm1122@gmail.com>
2020-05-16 20:22:35 +0200

    MongoDB version

--- src/main/java/com/simplemethod/university/MyCommand.java
+++ src/main/java/com/simplemethod/university/MyCommand.java
@@ -1,7 +1,7 @@
 package com.simplemethod.university;
 
 
-import com.simplemethod.university.couchbaseDAO.CouchbaseLecturerDAO;
+1
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,21 +11,12 @@ import picocli.CommandLine;
 @CommandLine.Command(name = "university", mixinStandardHelpOptions = true, version = "1.0 dev")
 public class MyCommand implements Runnable {
 
-
-    @Autowired
-    CouchbaseConfig couchbaseConfig;
-
-    @Autowired
-    CouchbaseLecturerDAO couchbaseLecturerDAO;
-
     @Autowired
     MongodbConfig mongodbConfig;
 
     @Autowired
     MainViewMongodb mainViewMongodb;
 
-    @Autowired
-    MainViewCouchbase mainViewCouchbase;
 
     @CommandLine.Option(names = {"-ip", "--ipaddress"}, description = "Server IP address.")
     private String[] ipAddress = new String[]{"localhost"};
@@ -39,46 +30,9 @@ public class MyCommand implements Runnable {
     @CommandLine.Option(names = {"-b", "--bucket"}, description = "Bucket name.")
     private String[] bucket = new String[]{"university","universityLecturer"};
 
-    @CommandLine.Option(names = {"-t", "--type"}, description = "Type of database M=MongoDB C=Couchbase")
-    private String[] type = new String[]{"C"};
-
     @SneakyThrows
     public void run() {
-
-        if(type[0].equals("C") || type[0].equals("c"))
-        {
-            if(bucket.length!=2)
-            {
-                System.out.println(CommandLine.Help.Ansi.AUTO.string("@|bold,fg(red) Brak wymaganych danych!|@"));
-                System.exit(1);
-            }
-
-            couchbaseConfig.init(ipAddress, login, password, bucket);
-            couchbaseLecturerDAO.init(ipAddress,login,password,bucket);
-            mainViewCouchbase.menu();
-        }
-        else if(type[0].equals("M") || type[0].equals("m"))
-        {
-            mongodbConfig.openConnection(ipAddress,bucket);
-            mainViewMongodb.menu();
-        }
-        else
-        {
-            System.out.println(CommandLine.Help.Ansi.AUTO.string("@|bold,fg(red) Nie wybrano żadnego skłądu!|@"));
-            System.exit(1);
-        }
-
-        // couchbase.init();
-
-        // couchbase.findAllByCurrentSemester();
-        //couchbase.setStudentEmailByAlbumNumber(85218,"m.mlodawski@simplemethod.io");
-        //couchbase.setStudentGradesByAlbumNumberAndSubjectNameAndIndependentStudyType(85218,"PSR",3);
-        // couchbase.findOneByAlbumNumber(85218);
-
-        // couchbase.findAllBySubjectsNameAndLectureGrades("PSR",6);
-
-
+        mongodbConfig.openConnection(ipAddress,bucket);
+        mainViewMongodb.menu();
     }
-
-
 }
